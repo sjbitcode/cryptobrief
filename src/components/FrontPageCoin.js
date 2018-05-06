a3e3b8bf03d94f67115afc437f3db9b85b535dae
Sangeeta Jadoonanan <sjbitcode@gmail.com>
2018-05-06 13:27:12 -0400

    Render partial news data and ticker data

--- src/components/FrontPageCoin.js
+++ src/components/FrontPageCoin.js
@@ -8,14 +8,42 @@ class FrontPageCoin extends React.Component {
     details: PropTypes.object
   };
 
-  render() {
-    const { details } = this.props;
+  renderTickerData = (coinObj) => {
+    const { ticker_data } = coinObj;
 
     return (
       <div>
-        <h3>{details.displayName}</h3>
-        <p>{details.price}</p>
-        <p>{details.news}</p>
+        <p>symbol: {ticker_data.symbol}</p>
+        <p>rank: {ticker_data.rank}</p>
+        <p>price_usd: {ticker_data.price_usd}</p>
+        <p>price_btc: {ticker_data.price_btc}</p>
+        <p>percent_change_24h: {ticker_data.percent_change_24h}</p>
+        <p>last_updated: {ticker_data.last_updated}</p>
+      </div>
+    );
+  };
+
+  renderNewsData = (coinObj) => {
+    const { news_data } = coinObj;
+    const first_article = news_data.articles[0];
+
+    return (
+      <div>
+        <p>{news_data.articles.length} articles found</p>
+        <strong>{first_article.title}</strong>
+        <i>{first_article.source.name}</i>
+      </div>
+    );
+  };
+
+  render() {
+    const { coin } = this.props;
+
+    return (
+      <div>
+        <h3>{coin.displayName}</h3>
+        {this.renderTickerData(coin)}
+        {this.renderNewsData(coin)}
       </div>
     );
   }
