c848c7891ef41f5df78a17ce5d20298eb1fa0089
Sangeeta Jadoonanan <sjbitcode@gmail.com>
2018-05-06 02:45:18 -0400

    Integrate newsapi into App. Display totalCount for now in DetailNews

--- src/components/App.js
+++ src/components/App.js
@@ -5,7 +5,8 @@ import Main from './Main';
 
 import coinList from '../coinList';
 import { getCoins, setCoins } from '../helpers/localCoins';
-import { fetchCoinMarketCap, fetchNewsApi } from '../api/crypto-compare';
+import { fetchCoinMarketCap } from '../api/coin-market-cap';
+import { fetchNewsApi } from '../api/news-api';
 
 
 class App extends Component {
@@ -166,8 +167,7 @@ class App extends Component {
     this.toggleTickerDataLoading(coinId, true);
     fetchCoinMarketCap(coinId)
     .then(data => {
-      // this.addOrUpdateTickerData(coinId, data.data[0], update);
-      this.addOrUpdateTickerData(coinId, data, update);
+      this.addOrUpdateTickerData(coinId, data.data[0], update);
     })
     .then(res => {
       this.toggleTickerDataLoading(coinId, false)
@@ -185,7 +185,7 @@ class App extends Component {
     this.toggleNewsDataLoading(coinId, true);
     fetchNewsApi(coinId)
     .then(data => {
-      this.addOrUpdateNewsData(coinId, data, update);
+      this.addOrUpdateNewsData(coinId, data.data, update);
     })
     .then(res => {
       this.toggleNewsDataLoading(coinId, false);
