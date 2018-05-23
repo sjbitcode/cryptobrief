2e26bbb8961add74ccfbb3b45735e26fe2883077
Sangeeta Jadoonanan <sjbitcode@gmail.com>
2018-05-23 04:32:55 -0400

    Lessen margin of main container in mobile

--- src/components/sidebar/NavBarChildren.js
+++ src/components/sidebar/NavBarChildren.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Container } from "semantic-ui-react";
 
 const NavBarChildren = ({ children }) => (
-  <Container style={{ marginTop: "5em" }}>{children}</Container>
+  <Container style={{ marginTop: "5em"}} id="mobileContainer">{children}</Container>
 );
 
 export default NavBarChildren;
